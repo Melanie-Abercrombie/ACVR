a1069bb61da8b744e2b129dab9ba766e075384eb
Melanie <melaniejabercrombie@gmail.com>
2019-01-31 16:49:43 -0800

    Alpha

---
+++ ACVR/Assets/02_Scripts/GrabberControls.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabberControls : MonoBehaviour {
+
+    private GameObject grabbedObject;
+    private bool grabbing;
+    public float grabRadius;
+
+	void GrabObject()
+    {
+        grabbing = true;
+        RaycastHit[] hits;
+
+        //Sphere of interaction around hand
+        hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, artwork);
+
+        //Collided
+        if (hits.Length > 0)
+        {
+            //Grabs closest object
+            int closestHit = 0;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].distance < closestHit.distance){
+                    closestHit = i;
+                }
+            }
+            grabbedObject = hits[closestHit].transform.gameObject;
+
+            grabbedObject.GetComponent<Rigidbody>.isKinematic = true;
+            grabbedObject.transform.position = transform.position;
+            //Follow hand
+        }
+    }
+
+    void DropObject()
+    {
+        grabbing = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Trigger pulled");
+        }
+
+        if(!grabbing && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) == 1)
+        {
+            GrabObject;
+        }
+        if(grabbing && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) < 1)
+        {
+            DropObject;
+        }
+
+        //Grab, point, press A - trigger raycast
+        if(!grabbing && (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) == 1) && (OVRINPUT.GetDown(OVRInput.Button))){
+
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //Separate function at all?
+        RaycastGrabber();
+        //SurfaceSnap(col);
+    }
+
+    // Points a ray to select an object in the distance
+    public void RaycastGrabber()
+    {
+
+        Debug.Log("Raycast engaged");
+    }
+}
---
+++ ACVR/Assets/02_Scripts/Snapping.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snapping : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Use spherecast?
+        RaycastHit hit;
+        float rayRange = Mathf.Infinity;
+        //Layermask floor art vs. wall art
+
+        /*if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayRange))
+        {
+
+        }*/
+
+        //Passed from RaycastGrabber
+
+        //If Raycast grabber hits surface,
+        //then is let go,
+        SurfaceSnap(hit.collider);
+    }
+
+    //Attaches an artwork to its appropriate surface when close
+    public void SurfaceSnap(Collider col)
+    {
+        if (col.CompareTag("WallArt"))
+        {
+            Debug.Log("Wall art");
+            Debug.Log(col.name);
+            //Sets artwork orientation to match the surface
+            //transform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+        }
+
+        if (col.CompareTag("FloorArt"))
+        {
+            Debug.Log("Floor art");
+            Debug.Log(col.name);
+            //Sets artwork orientation to match the surface
+            //transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
+
+        //Physics raycast small distance - get normal of what is hit - determines forward-facing vector
+    }
+}
